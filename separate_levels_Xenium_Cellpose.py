d3dc2ecd43584bcfc1ca922cdf418f66772c36b6
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2024-08-15 13:56:24 +0200

    Python script to separate tif image levels for cellpose

---
+++ separate_levels_Xenium_Cellpose.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# python script to separate the main morphology image of Xenium into separate levels
+# so that cellpose can process this
+
+import tifffile
+import sys, os
+
+tiff = sys.argv[1]
+outpath = os.path.dirname(tiff)
+
+# Variable 'LEVEL' determines the level to extract. It ranges from 0 (highest
+# resolution) to 6 (lowest resolution) for morphology.ome.tif
+LEVEL = 1   # highest resolution 0 did not work, so gave 1
+
+with tifffile.TiffFile(tiff) as tif:
+    image = tif.series[0].levels[LEVEL].asarray()
+
+tifffile.imwrite(outpath+'/cellpose_level_'+str(LEVEL)+'_morphology.ome.tif',
+    image,
+    photometric='minisblack',
+    dtype='uint16',
+    tile=(1024, 1024),
+    compression='JPEG_2000_LOSSY',
+    metadata={'axes': 'ZYX'},
+)
+
+
