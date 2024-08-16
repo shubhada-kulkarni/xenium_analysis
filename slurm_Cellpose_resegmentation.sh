3fe5de6df48027a2806e0d5407e7201b8345c1cc
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2024-08-16 15:32:56 +0200

    Running cellpose + transcript mapping in one script

--- slurm_Cellpose_resegmentation.sh
+++ slurm_Cellpose_resegmentation.sh
@@ -4,10 +4,10 @@
 #SBATCH --mail-user=shubhada.kulkarni@uni-heidelberg.de
 #SBATCH --error="%x.err.%j"
 #SBATCH --output="%x.out.%j"
-#SBATCH --gres=gpu:ampere:2
-#SBATCH --partition=gpu
+# #SBATCH --gres=gpu:pascal:1
+# #SBATCH --partition=gpu
 #SBATCH --mem=100G
-
+# #SBATCH --nodelist=gpu-g2-1
 
 # arguments
 # 1. folder with Xenium output for which the segmentation needs to be done
@@ -19,5 +19,12 @@ path=`dirname $in_folder`
 
 cd $path
 
-time  python -m cellpose --dir $in_folder --train --chan 0 --chan2 0 --diameter 10 --do_3D --save_tif --verbose --img_filter morphology.ome --save_tif --use_gpu
+## training
+# time  python -m cellpose --dir $in_folder --train --chan 0 --chan2 0 --diameter 10 --do_3D --save_tif --verbose --img_filter _morphology.ome --save_tif --use_gpu --mask_filter "_seg.npy"
 
+# time python -m cellpose --dir $1 --pretrained_model cyto --chan 0 --chan2 0 --diameter 10 --do_3D --save_tif --verbose --img_filter _6_morphology.ome
+
+
+# after this command is successful, also run the map transcript function to create cell-feature matrix
+cd $in_folder 
+python /prj/XeniumProbeDesign/analysis_scripts/map_transcripts_Cellpose.py -cellpose cellpose_level_6_morphology.ome_seg.npy -transcript transcripts.parquet -out feature_cell_matrix -pix_size 1.7
