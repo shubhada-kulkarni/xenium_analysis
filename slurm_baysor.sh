b623c8de928a177df4e751bac07703fe3e06dcf6
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2024-10-25 15:27:42 +0200

    Added confidence score as an argument and fixed import segmentation command

--- slurm_baysor.sh
+++ slurm_baysor.sh
@@ -8,6 +8,8 @@
 #SBATCH --mem=100G
 
 xenium_folder=$1
+confidence=$2
+seg_image=$3
 
 date
 cd /prj/XeniumProbeDesign/analysis_scripts # cd to folder where transcript file exists
@@ -21,21 +23,33 @@ echo "formatting the CSV file"
 file_transcript=$xenium_folder"/transcripts.csv"
 out_formatted=$xenium_folder"/formatted_transcripts.csv"
 echo $file_transcript, $out_formatted
-# first format the input transcript csv file (unzipped)
-python filter_transcripts_Baysor.py -transcript $file_transcript -out $out_formatted
-date
+# first check if formatted the input transcript csv file (unzipped)exists and if not, generate one
 
-echo "running baysor"
+if [ ! -f $out_formatted ]; then
+    echo "File not found! Running filter transcript script to generated formatted transcript CSV file."
+    python filter_transcripts_Baysor.py -transcript $file_transcript -out $out_formatted
+    date
+fi
+
+echo "Running baysor"
 # next supply this formatted output to baysor run
-baysor_out=$xenium_folder"/baysor"
-~/.julia/bin/baysor run -x x_location -y y_location -z z_location -g feature_name -m 10 -p --prior-segmentation-confidence 0.1 $out_formatted :cell_id_int --save-polygons GeoJSON -o $baysor_out
+baysor_out=$xenium_folder"/baysor"$confidence
+mkdir $baysor_out
+
+# ~/.julia/bin/baysor run -x x_location -y y_location -z z_location -g feature_name -m 10 -p --prior-segmentation-confidence $confidence $out_formatted :cell_id_int --save-polygons GeoJSON -o $baysor_out
+
+~/.julia/bin/baysor run $out_formatted $seg_image -x x_location -y y_location -z z_location -g feature_name -m 20 --prior-segmentation-confidence $confidence --save-polygons GeoJSON -o $baysor_out
 date
 
 echo "Importing segmentations"
 cd $xenium_folder
-xeniumranger import-segmentation --id=baysor-segmented --xenium-bundle=$xenium_folder --transcript-assignment=$baysor_out"/segmentation.csv" --viz-polygons=$baysor_out"/segmentation_polygons.json" --units=microns --localcores=32 --localmem=128
+
+out_xenium="baysor-segmented-"`echo "$confidence" | sed 's/\.//'`
+xeniumranger import-segmentation --id=$out_xenium --xenium-bundle=$xenium_folder --transcript-assignment=$baysor_out"/segmentation.csv" --viz-polygons=$baysor_out"/segmentation_polygons.json" --units=microns --localcores=32 --localmem=128
 date
 echo "Done"
+
+mv $baysor_out $out_xenium
 
 echo "Re-zipping the transcript CSV file"
 gzip $file_transcript
